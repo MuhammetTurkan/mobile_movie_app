2a44137155eaa025e8128e9af527a9a3dde0b483
MuhammetTurkan <muhammet.trkn98@gmail.com>
2025-04-19 19:44:46 +0300

    add: add the new color themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
     extend: {
       colors: {
         primary: "#93c47d",
+        secondary: "#151312",
+        light: {
+          100: "#D6C6FF",
+          200: "#A8B5DB",
+          300: "#9CA4AB",
+        },
+        dark: {
+          100: "#221F3D",
+          200: "#0F0D23",
+        },
+
         accent: "#AB8BFF",
       },
     },
