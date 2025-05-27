f1cb17ac7313e4814796bec6aca670a7b4ae5029
MuhammetTurkan <muhammet.trkn98@gmail.com>
2025-05-27 21:50:53 +0300

    add: add the ActivityIndicator component in detail screen

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -5,6 +5,7 @@ import {
   View,
   Image,
   TouchableOpacity,
+  ActivityIndicator,
 } from "react-native";
 import React, { useCallback, useEffect, useState } from "react";
 import { router, useLocalSearchParams } from "expo-router";
@@ -50,9 +51,11 @@ export default function MovieDetails() {
     [isOnList]
   );
 
-  const { data: movie, loading } = useFetch(() =>
-    fetchMovieDetails(id as string)
-  );
+  const {
+    data: movie,
+    loading: movieLoading,
+    error: movieError,
+  } = useFetch(() => fetchMovieDetails(id as string));
 
   useEffect(() => {
     async function fetchFavorite() {
@@ -97,80 +100,93 @@ export default function MovieDetails() {
           paddingBottom: 100,
         }}
       >
-        <View>
-          <Image
-            source={{
-              uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
-            }}
-            className="w-full h-[550px]"
-            resizeMode="stretch"
+        {movieLoading ? (
+          <ActivityIndicator
+            size={"large"}
+            color={"#0000ff"}
+            className="mt-10 self-center"
           />
-        </View>
-        <View className="flex-col items-start justify-center mt-5 px-5">
-          <View className="flex-row items-center justify-between w-full">
-            <Text className="text-white font-bold text-xl w-[50%]">
-              {movie?.title}
-            </Text>
-            <TouchableOpacity
-              className="bg-slate-500 px-2 py-2 rounded-xl mt-1"
-              onPress={() => togglePress(movie?.id as any)}
-            >
-              {isOnList ? (
-                <Text className="text-white font-bold text-md">
-                  Remove the Favorite List
+        ) : movieError ? (
+          <Text>Error : {movieError.message}</Text>
+        ) : (
+          <>
+            <View>
+              <Image
+                source={{
+                  uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
+                }}
+                className="w-full h-[550px] rounded-b-3xl"
+                resizeMode="stretch"
+              />
+            </View>
+            <View className="flex-col items-start justify-center mt-5 px-5">
+              <View className="flex-row items-center justify-between w-full">
+                <Text className="text-white font-bold text-xl w-[50%]">
+                  {movie?.title}
                 </Text>
-              ) : (
-                <Text className="text-white font-bold text-md">
-                  Add the Favorite List
-                </Text>
-              )}
-            </TouchableOpacity>
-          </View>
+                <TouchableOpacity
+                  className="bg-slate-500 px-2 py-2 rounded-xl mt-1"
+                  onPress={() => togglePress(movie?.id as any)}
+                >
+                  {isOnList ? (
+                    <Text className="text-white font-bold text-md">
+                      Remove the Favorite List
+                    </Text>
+                  ) : (
+                    <Text className="text-white font-bold text-md">
+                      Add the Favorite List
+                    </Text>
+                  )}
+                </TouchableOpacity>
+              </View>
 
-          <View className="flex-row items-center gap-x-1 mt-2">
-            <Text className="text-light-200 text-sm">
-              {movie?.release_date.split("-")[0]} {movie?.runtime}m
-            </Text>
-          </View>
-          <View className="flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2">
-            <Image source={icons.star} className="size-4" />
-            <Text className="text-white font-bold text-sm">
-              {Math.round(movie?.vote_average ?? 0)} / 10
-            </Text>
-            <Text className="text-light-200 text-sm">
-              ({movie?.vote_count} votes)
-            </Text>
-          </View>
-          <MovieInfo label="Overview" value={movie?.overview} />
-          <MovieInfo
-            label="Genres"
-            value={movie?.genres?.map((g) => g.name).join(` - `) || "N/A"}
-          />
-          <View className="flex flex-row justify-between w-1/2">
-            <MovieInfo
-              label="Budget"
-              value={`$${movie?.budget / 1_000_000} millions`}
-            />
-            <MovieInfo
-              label="Revenue"
-              value={`$${Math.round(movie?.revenue) / 1_000_000}`}
-            />
-          </View>
-          <MovieInfo
-            label="Production Companies"
-            value={
-              movie?.production_companies.map((c) => c.name).join(" - ") ||
-              `N/A`
-            }
-          />
-          <MovieInfo
-            label="Production Countries"
-            value={
-              movie?.production_countries.map((co) => co.name).join(" - ") ||
-              `N/A`
-            }
-          />
-        </View>
+              <View className="flex-row items-center gap-x-1 mt-2">
+                <Text className="text-light-200 text-sm">
+                  {movie?.release_date.split("-")[0]} {movie?.runtime}m
+                </Text>
+              </View>
+              <View className="flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2">
+                <Image source={icons.star} className="size-4" />
+                <Text className="text-white font-bold text-sm">
+                  {Math.round(movie?.vote_average ?? 0)} / 10
+                </Text>
+                <Text className="text-light-200 text-sm">
+                  ({movie?.vote_count} votes)
+                </Text>
+              </View>
+              <MovieInfo label="Overview" value={movie?.overview} />
+              <MovieInfo
+                label="Genres"
+                value={movie?.genres?.map((g) => g.name).join(` - `) || "N/A"}
+              />
+              <View className="flex flex-row justify-between w-1/2">
+                <MovieInfo
+                  label="Budget"
+                  value={`$${movie?.budget / 1_000_000} millions`}
+                />
+                <MovieInfo
+                  label="Revenue"
+                  value={`$${Math.round(movie?.revenue) / 1_000_000}`}
+                />
+              </View>
+              <MovieInfo
+                label="Production Companies"
+                value={
+                  movie?.production_companies.map((c) => c.name).join(" - ") ||
+                  `N/A`
+                }
+              />
+              <MovieInfo
+                label="Production Countries"
+                value={
+                  movie?.production_countries
+                    .map((co) => co.name)
+                    .join(" - ") || `N/A`
+                }
+              />
+            </View>
+          </>
+        )}
       </ScrollView>
       <TouchableOpacity
         onPress={router.back}
